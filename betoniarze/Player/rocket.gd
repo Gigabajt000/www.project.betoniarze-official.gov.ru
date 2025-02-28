1c4070bdeb12db1e55629863fde260b380e00d29
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-02-28 21:25:38 +0100

    nie działa ale daję

--- betoniarze/Player/rocket.gd
+++ betoniarze/Player/rocket.gd
@@ -1,17 +1,26 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var speed: float = 800
 @export var explosion_range: float = 100
 @export var max_explosion_force: float = 800
+var shoot_direction: Vector2 = Vector2.ZERO
 
 func _ready():
-	linear_velocity = transform.x * speed  # Launch the rocket
+	velocity = shoot_direction * speed
+	
+func _physics_process(delta: float) -> void:
+	move_and_slide()
 
 func _on_body_entered(body):
-	explode()
+	if body.is_in_group("ground"):
+		explode()
 
 func explode():
-	for body in $"Rocket Area".get_overlapping_bodies():
+	var explosion = preload("res://Player/explosion.tscn").instantiate()
+	explosion.global_position = global_position
+	get_parent().add_child(explosion)  # Add explosion effect
+
+	for body in $"Rocket Area2".get_overlapping_bodies():
 		if body is CharacterBody2D:
 			body.rocket_jump(global_position, max_explosion_force, explosion_range)
 	
