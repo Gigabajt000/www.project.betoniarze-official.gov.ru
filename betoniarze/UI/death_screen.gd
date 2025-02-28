ee33801f100509714807ab8918031d7dbbc1d209
noone787 <witcherandmaster@op.pl>
2025-02-28 20:40:49 +0100

    death_screen

---
+++ betoniarze/UI/death_screen.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_wyjdź_pressed():
+	get_tree().quit()
+
+
+func _on_zagraj_ponownie_pressed():
+	get_tree().change_scene_to_file("res://World/main.tscn")
+
+
+func _on_main_menu_pressed():
+	pass # Replace with function body.
