6ca064f30306043d6e6048773b2d6cf3d1609229
noone787 <witcherandmaster@op.pl>
2025-03-01 12:12:06 +0100

    particle system eksplozji

--- betoniarze/Player/explosion.gd
+++ betoniarze/Player/explosion.gd
@@ -4,8 +4,9 @@ extends Area2D
 func _physics_process(delta):
 	for o in get_overlapping_bodies():
 		if o is RigidBody2D:
+			$CPUParticles2D.emitting = true
 			var force = (o.global_position - global_position).normalized()
-			force *= 200
+			force *= 100
 			o.apply_central_impulse(force)
 
 
