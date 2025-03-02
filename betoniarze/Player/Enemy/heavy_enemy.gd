40f06be7a69af4d9ba8216babedaae6537a4ac60
noone787 <witcherandmaster@op.pl>
2025-03-02 20:33:03 +0100

    heavy enemy skonczony

--- betoniarze/Player/Enemy/heavy_enemy.gd
+++ betoniarze/Player/Enemy/heavy_enemy.gd
@@ -13,31 +13,17 @@ var direction
 var can_move = true
 var SPEED = 60
 
+
+
 func _physics_process(delta: float) -> void:
 	
 	cos3 = raycast_dół_lewo.get_collider()
 	cos4 = raycast_dół1_prawo.get_collider()
 	
-	
-	
-	if cos3 == null and temporary_direction > 0 :
-		can_move = false
-	if cos4 == null and temporary_direction < 0 :
-		can_move = false
-		
-	if cos3 == null and temporary_direction < 0 :
-		can_move = true
-	if cos4 == null and temporary_direction > 0:
-		can_move = true
 
-	if Global.IsAlive == true:
-		temporary_direction = (target.position.x - position.x)
-	
-	if can_move == false:
-		direction = 0
-	if temporary_direction > 15 and can_move == true:
+	if cos3 == null and can_move == true:
 		direction = 1
-	if temporary_direction < -15 and can_move == true:
+	if cos4 == null  and can_move == true:
 		direction = -1
 	
 	
@@ -45,3 +31,13 @@ func _physics_process(delta: float) -> void:
 		linear_velocity.x = SPEED * direction
 	else:
 		linear_velocity.x = move_toward(linear_velocity.x, 0, SPEED)
+
+func attack():
+	Global.Death()
+	
+	#animated_sprite.play("attack")
+	
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("Player"):
+		attack()
