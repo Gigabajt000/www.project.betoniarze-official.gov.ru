91ff617da9a41069525a88744b8cf76c2f50168f
noone787 <witcherandmaster@op.pl>
2025-03-02 19:16:16 +0100

    turret 2/3

--- betoniarze/Player/Turret/turret.gd
+++ betoniarze/Player/Turret/turret.gd
@@ -1,7 +1,7 @@
-extends RigidBody2D
+extends StaticBody2D
 
 
-var attacking: bool = true
+var attacking: bool = false
 var timer: float = 0
 
 @export var progress_bar: ProgressBar
@@ -9,17 +9,22 @@ var timer: float = 0
 var bullet_path = preload("res://Player/Turret/bullet_turret.tscn")
 
 func _ready():
-	progress_bar.max_value = 2
+	progress_bar.max_value = 3
 	
 	
 func _physics_process(delta):
-	
+	for o in $Area2D.get_overlapping_bodies():
+		if o.has_method("Player"):
+			
+			attacking = true
+
+		
 	progress_bar.value = timer
 	
 	if attacking == true:
 		timer = timer + delta
 	
-	if attacking == true and timer >= 2:
+	if attacking == true and timer >= 3:
 		atak()
 		timer = 0
 
@@ -30,11 +35,7 @@ func atak():
 	bullet.pos = $Node2D.global_position
 	get_parent().add_child(bullet)
 
-
-func _on_area_2d_body_entered(body):
-	if body.has_method("Player"):
-		attacking = true
-
+	
 
 func _on_area_2d_body_exited(body):
 	if body.has_method("Player"):
