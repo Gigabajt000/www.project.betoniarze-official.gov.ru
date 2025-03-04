ae1103957eb68de518f608cdbed0885210dc5dd5
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-03-04 18:51:03 +0100

    fade in and out

---
+++ betoniarze/UI/Fade_In/fade_in.gd
@@ -0,0 +1,22 @@
+extends ColorRect
+
+signal on_transition_finished
+
+@onready var color_rect = $ColorRect
+@onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+	color_rect.visible = false
+	animation_player.animation_finished.connect(_on_animation_finished)
+	
+func _on_animation_finished(anim_name):
+	if anim_name == "fade to black":
+		animation_player.play("fade to normal")
+		on_transition_finished.emit()
+	if anim_name == "fade to normal":
+		color_rect.visible = false
+	
+func transition():
+	color_rect.visible = true
+	animation_player.play("fade to black")
+	
