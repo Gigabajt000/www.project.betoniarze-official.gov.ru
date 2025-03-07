6f9396525b9a6df43b6a5562cbf4d7cfc59cd5f4
noone787 <witcherandmaster@op.pl>
2025-03-07 13:09:42 +0100

    hsfhg

--- betoniarze/Player/Beczka_Wybuchowa/beczka_wybuchowa.gd
+++ betoniarze/Player/Beczka_Wybuchowa/beczka_wybuchowa.gd
@@ -10,15 +10,23 @@ func _process(delta):
 		x = x + delta
 	if x >= 0.5:
 		$".".queue_free()
+	
 
+var emit_sound = true
 func _on_area_2d_area_entered(area):
+	if emit_sound == true:
+		$AudioStreamPlayer.play(0)
+		emit_sound = false
+		
 	if area.has_method("Eksplozja"):
 		var explosion_instance = explosion.instantiate()
 		add_child(explosion_instance)
 		explosion_instance.position = Vector2(0,0)
+		
 		$CPUParticles2D.emitting = true
 		$Sprite2D.visible = false
 		start_timer = true
+		
 		
 
 	
--- betoniarze/Player/Beczka_Wybuchowa/eksplozja_beczki.gd
+++ betoniarze/Player/Beczka_Wybuchowa/eksplozja_beczki.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
+
 func Eksplozja():
 	pass #to nic nie robi, ale zostaw
 
 
 func _physics_process(delta):
+	
+	
+		
 	for o in get_overlapping_bodies():
 		if o.has_method("Player"):
 			
