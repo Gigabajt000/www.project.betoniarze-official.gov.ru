c6c4942d0b92546be50b3684924fee932f512f93
noone787 <witcherandmaster@op.pl>
2025-03-07 17:30:39 +0100

    duzio rzeczy

--- betoniarze/UI/Level Selection/grid_container.gd
+++ betoniarze/UI/Level Selection/grid_container.gd
@@ -9,3 +9,16 @@ func _process(delta: float) -> void:
 	var viewport_height = get_viewport_rect().size.y
 	var y = viewport_height / 3 - $"Level 1".size.y 
 	add_theme_constant_override("v_separation", int(y))
+
+	if Global.level1 == true:
+		$"Level 1/Sprite2D".visible = false
+	if Global.level2 == true:
+		$"Level 2/Sprite2D".visible = false
+	if Global.level3 == true:
+		$"Level 3/Sprite2D".visible = false
+	if Global.level4 == true:
+		$"Level 4/Sprite2D".visible = false
+	if Global.level5 == true:
+		$"Level 5/Sprite2D".visible = false
+	if Global.level6 == true:
+		$"Level 6/Sprite2D".visible = false
--- betoniarze/global.gd
+++ betoniarze/global.gd
@@ -34,12 +34,11 @@ var stun : bool = false
 
 var level1: bool = true
 var level2: bool = true
-var level3: bool = true
-var level4: bool = true
-var level5: bool = true
-var level6: bool = true
-var level7: bool = true
-var level8: bool = true
+var level3: bool = false
+var level4: bool = false
+var level5: bool = false
+var level6: bool = false
+
 
 var change:bool
 
